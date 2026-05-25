2168bd28250b3e00cf1f61d0724cc41a547068f4
jorgegonzalezmata25-coder <jorge.gonzalez.mata25@gmail.com>
2026-05-25 21:42:46 +0000

    Force redeploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,3 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+// force deploy Mon May 25 21:42:46 UTC 2026
